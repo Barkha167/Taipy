20ac9bc2e1e96097f25a79ccd36d61affa97b0de
Barkha Verma <barkha.verma@crossml.com>
2024-11-14 12:08:23 +0530

    updated main.py and requirements.txt

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from dotenv import load_dotenv
 tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-base")
 model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-base")
 
+load_dotenv()
+api_key = os.getenv("hf")
+
 
 
 # Initial context and sample conversation
